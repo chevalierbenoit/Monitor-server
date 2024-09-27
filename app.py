e1e97e22c9b688e8db0442314db67509f411c7c2
Benoit Chevalier <benoit.chevalier@gmail.com>
2024-09-27 08:57:51 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,69 @@
+from pysnmp.hlapi.v3arch.asyncio import *
+from pystray import Icon, Menu, MenuItem
+from PIL import Image, ImageDraw
+import threading
+import asyncio
+
+# Fonction pour créer une icône de couleur
+def create_image(color):
+    image = Image.new('RGB', (64, 64), color)
+    draw = ImageDraw.Draw(image)
+    draw.ellipse((0, 0, 63, 63), outline="black", fill=color)
+    return image
+
+# Fonction pour vérifier l'état du serveur via SNMP
+async def check_snmp_status():
+    transport_target = await UdpTransportTarget.create(('192.168.0.250', 161))
+
+    error_indication, error_status, error_index, var_binds = await getCmd(
+        SnmpEngine(),
+        CommunityData('public', mpModel=0),  # SNMP v1, changez si nécessaire
+        transport_target,
+        ContextData(),
+        ObjectType(ObjectIdentity('1.3.6.1.2.1.1.3.0'))  # OID pour sysUptime
+    )
+
+    if error_indication:
+        print(f"Error indication: {error_indication}")
+        return None  # Retourne None en cas d'erreur
+    elif error_status:
+        print(f"Error status: {error_status.prettyPrint()}")
+        return None  # Retourne None en cas d'erreur
+    else:
+        # Extraction de l'uptime
+        uptime_ticks = var_binds[0][1]  # La valeur est le deuxième élément du tuple
+        return uptime_ticks  # Retourne les ticks d'uptime
+
+# Fonction de mise à jour de l'icône dans la barre des tâches
+async def update_icon(icon):
+    while True:
+        uptime = await check_snmp_status()
+        if uptime is not None:
+            print(f"uptime {uptime}")
+            # Convertir les ticks en secondes
+            uptime_seconds = uptime // 10  # Chaque tick est 10 ms
+            uptime_display = f'Uptime: {uptime_seconds // 3600}h {(uptime_seconds % 3600) // 60}m'
+            icon.icon = create_image('green')
+            icon.title = uptime_display  # Met à jour le tooltip avec l'uptime
+        else:
+            icon.icon = create_image('red')
+            icon.title = "Server Offline"  # Message d'erreur si le serveur est hors ligne
+
+        await asyncio.sleep(10)  # Vérifier toutes les 10 secondes
+
+# Fonction de démarrage de l'icône
+def setup(icon):
+    icon.visible = True
+    # Démarrage de la tâche asynchrone dans un thread séparé
+    loop = asyncio.new_event_loop()
+    threading.Thread(target=loop.run_until_complete, args=(update_icon(icon),)).start()
+
+
+# Menu contextuel (option pour quitter)
+menu = Menu(MenuItem('Quitter', lambda icon: icon.stop()))
+
+# Créer l'icône rouge par défaut
+icon = Icon("SNMP Status", create_image('red'), menu=menu)
+
+# Démarrage de l'icône dans la barre des tâches
+icon.run(setup)
